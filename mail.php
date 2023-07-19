54e695f32a0db83dad951b7619089a8aa1d70b08
LilRamzay13 <68799910+LilRamzay13@users.noreply.github.com>
2023-07-19 08:11:45 +0300

    ?

--- mail.php
+++ mail.php
@@ -19,7 +19,7 @@ $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, 
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
 $mail->setFrom('support@luch25.ru'); // от кого будет уходить письмо?
-$mail->addAddress('ooo_luch_25@mail.ru');     // Кому будет уходить письмо 
+$mail->addAddress('niks97@inbox.ru');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
